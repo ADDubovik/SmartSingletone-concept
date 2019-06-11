2e63664cc5703c432a8e8ab4cd32ce35055c4cc5
Alexander Dubovik <dubovik_a_d@mail.ru>
2019-06-11 16:34:49 +0300

    Renaming and advance

---
+++ src/Classic02_incorrect.cpp
@@ -0,0 +1,39 @@
+#include "SingletoneClassic.h"
+
+#include <memory>
+
+
+class ClassicSingleThreadedUser
+{
+public:
+  ClassicSingleThreadedUser() = default;
+
+  ~ClassicSingleThreadedUser()
+  {
+    auto &instance = SingletoneClassic::instance();
+    for ( int i = 0; i < 100; ++i )
+      instance.add(i);
+  }
+};
+
+
+void createStaticUser()
+{
+  static auto user = ClassicSingleThreadedUser();
+}
+
+
+int main()
+{
+  // 1. Create user
+  createStaticUser();
+  // 2. Create singletone
+  SingletoneClassic::instance();
+
+  // This will cause destruction in order:
+  // - singletone;
+  // - user.
+  // This order is incorrect.
+
+	return 0;
+}
