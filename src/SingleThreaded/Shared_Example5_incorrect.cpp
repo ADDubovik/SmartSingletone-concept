1de1cc79946700cc57a20233cd375b90302c6693
Alexander Dubovik <dubovik_a_d@mail.ru>
2019-06-15 22:25:04 +0300

    Advance

--- src/SingleThreaded/Shared_Example5_incorrect.cpp
+++ src/SingleThreaded/Shared_Example5_incorrect.cpp
@@ -3,11 +3,41 @@
 #include "SomethingWithVeryImportantDestructor.h"
 
 
+class SharedSingleThreadedUtility
+{
+public:
+  SharedSingleThreadedUtility()
+      // To ensure that singleton will be constucted before utility
+      : m_singleton(SingletonShared<CallbackPayload>::instance())
+  {
+    std::cout << "SharedSingleThreadedUtility()" << std::endl;
+  }
+
+  ~SharedSingleThreadedUtility()
+  {
+    std::cout << "~SharedSingleThreadedUtility()" << std::endl;
+  }
+
+  void setCallback(std::function<void()> &&fn)
+  {
+    if ( m_singleton )
+      m_singleton->setCallback(std::move(fn));
+  }
+
+private:
+  // A copy of smart pointer, not a reference
+  std::shared_ptr<SingletonShared<CallbackPayload>> m_singleton;
+};
+
+
+
 int main()
 {
+  auto utility = std::make_shared<SharedSingleThreadedUtility>();
+
   SingletonShared<CallbackPayload>::instance()->setCallback(
       [
-        capture = SingletonShared<CallbackPayload>::instance(),
+        utility,
         something = std::make_shared<SomethingWithVeryImportantDestructor>()
       ]
       ()
